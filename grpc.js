902ade57e77ae0cf4ee3914c5e232fd2d1ab48ce
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-02-13 12:01:03 +0700

    grpc-caller

--- grpc.js
+++ grpc.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser= require('body-parser');
-<<<<<<< HEAD
 
 const uuidV4 = require('uuid/v4');
 const caller = require('grpc-caller');
@@ -28,28 +27,6 @@ app.post('/quotes', (req, res) => {
 
 // Works as standard gRPC client
 app.get('/g1', (req, res) => {
-=======
-const caller = require('grpc-caller');
-const app = express();
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-const path = require('path');
-const appDir = path.dirname(require.main.filename);
-const PROTO_PATH = path.resolve(appDir, './proto/helloworld.proto')
-const client = caller('0.0.0.0:50051', PROTO_PATH, 'Greeter')
-
-var uuidV4 = require('uuid/v4');
-
-app.get('/demo', (req, res) => res.send('Hello World!'));
-
-app.get('/', (req, res) => {
-	res.sendFile(appDir + '/index.html');
-});
-
-app.get('/g1', (req, res) => {
-	console.log('G1 ...');
->>>>>>> 21754a9220c0a8920ef4f045a573ba4523f4273b
 	client.sayHello({ name: 'G1-' + uuidV4() }, (err, msg) => {
 		console.log(msg);
 		res.send(msg);
@@ -57,7 +34,6 @@ app.get('/g1', (req, res) => {
 	});
 });
 
-<<<<<<< HEAD
 // For request / response calls, also promisified if callback is not provided "then()".
 // Which means means you can use is with async / await
 app.get('/g2', (req, res) => {
@@ -75,21 +51,3 @@ app.get('/g2', (req, res) => {
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
-=======
-app.get('/g2', (req, res) => {
-	console.log('G2 ...');
-	const msg = await client.sayHello({ name: 'G2-' + uuidV4() })
-	console.log(msg);
-	res.send(msg);
-	res.end('');
-});
-
-
-
-app.post('/quotes', (req, res) => {
-  console.log(req.body)
-	res.end('');
-});
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
->>>>>>> 21754a9220c0a8920ef4f045a573ba4523f4273b
