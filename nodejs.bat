f7a670bf0f7763ff21f35da9ba28d94cc835ef30
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-02-21 21:55:15 +0700

    a

--- nodejs.bat
+++ nodejs.bat
@@ -1,6 +1,12 @@
 @ECHO OFF
 
+set app=%1
+
 taskkill /F /IM node.exe
-start C:\m2\App\msys32\usr\bin\node.exe E:\nodejs\app.js
+start C:\m2\App\msys32\usr\bin\node.exe E:\nodejs\%app%.js
 
 tasklist /fi "imagename eq node.exe"
+
+echo.
+echo [ RUNING: %app%.js at http://localhost ...]
+echo.
